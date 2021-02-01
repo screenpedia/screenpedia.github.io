3f1a97b7732851019784af93244d8d6297e0e64a
Matias Obezzi <matiasobezzi@gmail.com>
2021-02-01 16:37:08 -0300

    [Home Banner] bottom right border radius added on mobile

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -21,6 +21,9 @@ const HomeBanner = styled.div`
     background: #667db6;  /* fallback for old browsers */
     background: -webkit-linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    @media(max-width: 991px){
+        border-bottom-right-radius: 10px;
+    }
 `,
 StyledForm = styled.form`
     margin-top: 10px;
